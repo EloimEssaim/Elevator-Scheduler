570cfac774c19c8add603f8b13b3e46369fec357
陈淇格 <cqg186495@outlook.com>
2017-05-02 14:36:45 +0800

    First version.Bugs not solved

---
+++ src/com/company/Elevator.java
@@ -0,0 +1,18 @@
+package com.company; /**
+ * Created by chrischen on 2017/4/27.
+ */
+
+import java.awt.*;
+import javax.swing.*;
+import java.lang.Runnable;
+
+public class Elevator implements Runnable
+{
+    public void run()
+    {
+
+    }
+}
+
+
+
